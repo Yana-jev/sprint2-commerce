82cc86ea59902febe53b1f807766c378d93b7e14
Yana Evko <evko.yana@gmail.com>
2024-06-02 13:04:22 +0200

    change checkout

--- js/shop.js
+++ js/shop.js
@@ -76,36 +76,162 @@ var total = 0;
 
 // Exercise 1
 function buy(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array
+
+let encontrado = false
+
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === id) {
+
+            for (let j = 0; j < cart.length; j++) {
+                if (cart[j].id === id) {
+                    
+                    cart[j].quantity++;
+                    encontrado = true;
+                    break;
+                }
+            }
+
+            // если нет в корзине, то добавдяем +1
+            if (!encontrado) {
+                let productToAdd = { ...products[i], quantity: 1 };
+                cart.push(productToAdd);
+            
+            }
+            break; 
+        }
+    }
+    updateCartCount();
 }
+
+// actualizar la cantidad de productos en el carrito
+function updateCartCount() {
+    const countElement = document.getElementById('count_product');
+    if (countElement) {
+        // Calcular la cantidad total sumando las cantidades de todos los productos en el carrito
+        const totalCount = cart.reduce((total, item) => total + item.quantity, 0);
+        countElement.textContent = totalCount;
+    }
+}
+
 
 // Exercise 2
 function cleanCart() {
+
+cart = [];
+console.log("Cart after cleaning:", cart);
+updateCartCount()
+printCart()
 
 }
 
 // Exercise 3
 function calculateTotal() {
+
+    
     // Calculate total price of the cart using the "cartList" array
+    let suma = 0;
+    for (let i = 0; i < cart.length; i++) {
+        suma += cart[i].price * cart[i].quantity;
+    }
+    console.log(suma)
+    return suma;
+
+
 }
 
 // Exercise 4
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+
+let discountOil = 0.80
+let discountCakeMix = 0.70
+
+    for (let i = 0; i < cart.length; i++) {
+        let product = cart[i];
+        if (product.name === 'Cooking Oil' && product.quantity >= 3) {
+            product.subtotalWithDiscount = product.price * product.quantity * discountOil;
+        } else if (product.name === 'Cake Mix' && product.quantity >= 10) {
+            product.subtotalWithDiscount = product.price * product.quantity * discountCakeMix;
+        } else {
+            delete product.subtotalWithDiscount;
+        }
+    }
+
 }
+
 
 // Exercise 5
 function printCart() {
-    // Fill the shopping cart modal manipulating the shopping cart dom
+
+    applyPromotionsCart()
+
+let listElement = document.getElementById('cart_list');
+listElement.innerHTML = '';     
+let totalPrice = 0;
+
+for (let i = 0; i < cart.length; i++) {
+    let product = cart[i];
+    let productTotal = product.price * product.quantity;
+    if (product.subtotalWithDiscount) {
+        productTotal = product.subtotalWithDiscount;
+    }
+    totalPrice += productTotal;
+
+    listElement.innerHTML += `
+        <tr>
+            <th scope="row">${product.name}</th>
+            <td>$${product.price.toFixed(2)}</td>
+            <td>${product.quantity}</td>
+            <td>$${productTotal.toFixed(2)}</td>
+            <td><button class="btn btn-small btn-primary" onclick="removeFromCart(${product.id})">-</button>
+            <td>${product.quantity}</td>
+            <td><button class="btn btn-small btn-primary" onclick="addToCard(${product.id})">+</button></td>
+        </tr>
+    `
 }
+
+const totalPriceElement = document.getElementById('total_price');
+if (totalPriceElement) {
+    totalPriceElement.innerHTML = `Total: $${totalPrice.toFixed(2)}`;
+} 
+}
+applyPromotionsCart();
+printCart();
+
+
 
 
 // ** Nivell II **
 
 // Exercise 7
 function removeFromCart(id) {
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].id === id) {
+            cart[i].quantity--;
+            if (cart[i].quantity === 0) {
+                cart.splice(i, 1);
+            }
+            break;
+        }
+}
 
+
+
+updateCartCount();
+    printCart();
+}
+
+
+
+function addToCard(id) {
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].id === id) {
+            cart[i].quantity++;
+            break;
+        }
+    }
+    updateCartCount();
+    printCart();
 }
 
 function open_modal() {
