8a0522437a97f2c2d0e47e7c4aaccef67b129f03
Yana Evko <evko.yana@gmail.com>
2024-06-03 12:27:55 +0200

    add errorname

--- js/checkout.js
+++ js/checkout.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		var fName = document.getElementById("fName");
 
 		if (fName.validity.patternMismatch) {
-			document.getElementById('errorName').innerHTML = 'Это поле обязательно к заполнению и должно содержать не менее 3 символов, состоящих только из букв';
+			document.getElementById('errorName').innerHTML = 'This field is required and must have, at least,3 characters';
 		} else {
 			document.getElementById('errorName').innerHTML = '';
 		}
